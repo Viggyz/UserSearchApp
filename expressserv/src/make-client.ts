94c9153ea77c7c5882708b0126b7b699adb59f15
Viggyz <devvig19@gmail.com>
2022-04-14 16:51:00 +0400

    Package missing

--- expressserv/src/make-client.ts
+++ expressserv/src/make-client.ts
@@ -1,8 +1,9 @@
 import { createClient } from 'redis';
 
-const url = process.env.URL || "127.0.0.1";
-const port = process.env.PORT || 6379;
+const url = process.env.REDIS_URL || "127.0.0.1";
 
-const redisClient = createClient({ url: `redis://${url}:${port}` });
+
+
+const redisClient = createClient({ url: `redis://${url}` });
 
 export { redisClient }
